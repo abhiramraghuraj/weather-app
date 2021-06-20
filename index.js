d936adf44a9dcd733647068213cbc00045a21894
Abhiram Raghuraj <abhi.77720@gmail.com>
2021-06-20 15:46:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+    const box = document.querySelector('.container');
+    const boxRect = box.getBoundingClientRect()
+
+    box.addEventListener('mousemove', e => {
+        const xp = (e.clientX - boxRect.left)/boxRect.width
+        const yp = (e.clientY - boxRect.top)/boxRect.height - 0.6
+        const xoff = -(xp - 0.5)
+        const dx = Math.min(Math.max(xoff, -0.6), 0.6)
+        box.style.transform = `perspective(10000px) rotateY(${dx*60}deg) rotateX(${yp*50}deg)`
+
+    })
+
+    box.addEventListener('mouseleave', () => {
+        box.style.transform = 'none'
+    })
+
+
+    //WEATHER
+    const api = {
+    key: "a1c8680006a301643c6d604aaf850cd7",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+var details = document.querySelector('.details');
+
+const searchbox = document.querySelector('.search');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(e){
+    if(e.keyCode == 13){
+        getResults(searchbox.value);
+        details.style.opacity = 1;
+        searchbox
+    }
+}
+
+function getResults(query){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder(d){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
